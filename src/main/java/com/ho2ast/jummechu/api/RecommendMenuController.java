5b859b3cf51eaf98046288891e8b19c53829d312
dh.nam <dh.nam@okestro.com>
2022-10-05 17:49:17 +0900

    보신다기에 살짝 정리

--- src/main/java/com/ho2ast/jummechu/api/RecommendMenuController.java
+++ src/main/java/com/ho2ast/jummechu/api/RecommendMenuController.java
@@ -16,12 +16,14 @@ public class RecommendMenuController {
     @PostMapping("/getMenu")
     public void getMenu() throws IOException {
 
+        // 페이지 랜덤 선택
+        // API에서 실제로는 음식적 목록이 3페이지 이상 조회되나 3페이지 이상 선택될 경우 3페이지로 고정됨.
         int pageNum = (int) (Math.random() * 3 + 1);
         String page = String.valueOf(pageNum);
 
         WebClient build = WebClient.builder()
                 .baseUrl("https://dapi.kakao.com")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "KAKAO API KEY")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "카카오 API 키")
                 .build();
 
         Map block = build.get()
@@ -43,33 +45,24 @@ public class RecommendMenuController {
         int listNum = new Random().nextInt(15);
         Map map = (Map) documents.get(listNum);
 
+        // webhook return template
         Map<String, Object> result = new HashMap<>();
         result.put("body", "[["+map.get("place_name")+"]]("+map.get("place_url")+")");
         result.put("connectColor", "#FAC11B");
 
         ArrayList<Object> list = new ArrayList<>();
-
-        Map<String, Object> placeName = new HashMap<>();
-        placeName.put("title", "식당");
-        placeName.put("description", map.get("place_name"));
+        Map<String, Object> placeName = Map.of("title", "식당", "description", map.get("place_name"));
+        Map<String, Object> category = Map.of("title", "분류","description", map.get("category_name"));
+        Map<String, Object> location = Map.of("title", "위치", "description", map.get("place_url"));
         list.add(placeName);
-
-        Map<String, Object> category = new HashMap<>();
-        category.put("title", "분류");
-        category.put("description", map.get("category_name"));
         list.add(category);
-
-        Map<String, Object> location = new HashMap<>();
-        location.put("title", "위치");
-        location.put("description", map.get("place_url"));
         list.add(location);
 
         result.put("connectInfo", list);
 
         String body = new Gson().toJson(result);
-
         WebClient.builder()
-                .baseUrl("Webhook 수신 URL")
+                .baseUrl("잔디 웹훅 키")
                 .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.tosslab.jandi-v2+json")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                 .build()
